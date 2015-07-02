5ccf8fdef3100dc646fd2e88e7fcaf963dc857c0
Genesis King <jxttian@aliyun.com>
2015-07-02 20:12:15 +0800

    添加注解

---
+++ core/src/main/java/net/myscloud/pandora/core/boot/Bootstrap.java
@@ -0,0 +1,16 @@
+package net.myscloud.pandora.core.boot;
+
+/**
+ * Created by user on 2015/7/2.
+ */
+public interface Bootstrap {
+    void start();
+
+    void stop();
+
+    void restart();
+
+    void status();
+
+    Bootstrap init();
+}
