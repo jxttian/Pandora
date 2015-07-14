27a65df79d5cf31bbb9ccf205ab3f99c18c17e6b
Genesis King <jxttian@aliyun.com>
2015-07-14 18:15:43 +0800

    添加 akka

--- http/src/main/java/net/myscloud/pandora/http/server/HttpServerInitializer.java
+++ http/src/main/java/net/myscloud/pandora/http/server/HttpServerInitializer.java
@@ -18,18 +18,39 @@ package net.myscloud.pandora.http.server;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.*;
 import io.netty.handler.stream.ChunkedWriteHandler;
 
 public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
 
     @Override
     public void initChannel(SocketChannel ch) {
+        // Create a default pipeline implementation.
         ChannelPipeline pipeline = ch.pipeline();
-        pipeline.addLast(new HttpServerCodec());
+
+        /**
+         * http-request解码器
+         * http服务器端对request解码
+         */
+        pipeline.addLast("decoder", new HttpRequestDecoder());
+        /**
+         * http-response解码器
+         * http服务器端对response编码
+         */
+        pipeline.addLast("encoder", new HttpResponseEncoder());
+
+        /**
+         * 压缩
+         * Compresses an HttpMessage and an HttpContent in gzip or deflate encoding
+         * while respecting the "Accept-Encoding" header.
+         * If there is no matching encoding, no compression is done.
+         */
+        pipeline.addLast("deflater", new HttpContentCompressor());
+
         pipeline.addLast(new HttpObjectAggregator(65536));
+
         pipeline.addLast(new ChunkedWriteHandler());
+
         pipeline.addLast(new HttpServerHandler());
     }
 }
